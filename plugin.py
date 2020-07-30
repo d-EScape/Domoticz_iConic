7afbad0a847e384307d17de7cdd627ebcf0eede0
d-EScape <8693608+d-EScape@users.noreply.github.com>
2020-07-30 18:59:38 +0200

    fixed typo

--- plugin.py
+++ plugin.py
@@ -58,7 +58,7 @@ class BasePlugin:
 #	def onCommand(self, Unit, Command, Level, Hue):
 		
 	def onStop(self):
-		Domoticz.Satus('IConic stopped')
+		Domoticz.Status('IConic stopped')
 
 global _plugin
 _plugin = BasePlugin()
